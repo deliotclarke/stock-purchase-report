248b6fff2ea27edf629095d780834d793cc60410
Daniel Clarke <deliotclarke@gmail.com>
2019-07-09 15:53:39 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stock_purchase_report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
